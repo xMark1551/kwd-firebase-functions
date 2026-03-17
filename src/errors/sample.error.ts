49279451bb06ca7f703936f2c64ca63b728fe229
Mark King <markkings1551@gmail.com>
2026-03-17 11:11:17 +0800

    nothing

--- src/errors/sample.error.ts
+++
@@ -1,7 +0,0 @@
-import { HttpError } from "../http/http.error";
-
-export class RateLimitError extends HttpError {
-  constructor(message = "Rate limit exceeded", meta?: unknown) {
-    super(400, message, "RATE_LIMIT", meta);
-  }
-}
